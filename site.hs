4bf2b349b8dde2da517cb0ffcb18f457c034b308
Ash <ashermanning@yahoo.co.uk>
2020-05-12 14:47:54 +0800

    Clean Urls

--- site.hs
+++ site.hs
@@ -1,11 +1,29 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NamedFieldPuns #-}
+import           Data.List (isSuffixOf)
 import           Data.Monoid (mappend)
+import           System.FilePath ((</>), takeDirectory, takeBaseName)
 import           Hakyll
 
-
 --------------------------------------------------------------------------------
+
+cleanRoute :: Routes
+cleanRoute = customRoute $ \ident ->
+    let fp = toFilePath ident in
+    takeDirectory fp </> takeBaseName fp </> "index.html"
+
+cleanIndex :: String -> String
+cleanIndex url
+    | idx `isSuffixOf` url = take (length url - length idx) url
+    | otherwise            = url
+  where idx = "index.html"
+
+cleanIndexUrls :: Item String -> Compiler (Item String)
+cleanIndexUrls = return . fmap (withUrls cleanIndex)
+
+cleanIndexHtmls :: Item String -> Compiler (Item String)
+cleanIndexHtmls = return . fmap (replaceAll "/index.html" (const "/"))
+
 
 configuration :: IO Configuration
 configuration = do
@@ -29,20 +47,20 @@ main = do
             compile compressCssCompiler
 
         match (fromList ["about.rst", "contact.markdown"]) $ do
-            route   $ setExtension "html"
+            route   cleanRoute
             compile $ pandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
         match "posts/*" $ do
-            route $ setExtension "html"
+            route $ cleanRoute
             compile $ pandocCompiler
                 >>= loadAndApplyTemplate "templates/post.html"    postCtx
                 >>= loadAndApplyTemplate "templates/default.html" postCtx
                 >>= relativizeUrls
 
         create ["archive.html"] $ do
-            route idRoute
+            route cleanRoute
             compile $ do
                 posts <- recentFirst =<< loadAll "posts/*"
                 let archiveCtx =
@@ -54,6 +72,7 @@ main = do
                     >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                     >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                     >>= relativizeUrls
+                    >>= cleanIndexUrls
 
 
         match "index.html" $ do
@@ -68,6 +87,7 @@ main = do
                     >>= applyAsTemplate indexCtx
                     >>= loadAndApplyTemplate "templates/default.html" indexCtx
                     >>= relativizeUrls
+                    >>= cleanIndexUrls -- cleanup href in all anchor tags.
 
         match "templates/*" $ compile templateBodyCompiler
 
